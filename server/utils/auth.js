3bfe0d73c392b90790bccbf22fd2c293e84cfe7e
Nathanael White <narhanaelw99@gmail.com>
2021-08-14 17:54:31 -0500

    updating package json files

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -4,11 +4,6 @@ const secret = "mysecretshhhhhh";
 const expiration = "2h";
 
 module.exports = {
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
-
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
   authMiddleware: function ({ req }) {
     let token = req.body.token || req.query.token || req.headers.authorization;
 
@@ -27,5 +22,10 @@ module.exports = {
     }
 
     return req;
+  },
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
